dfe1fc8bc9f46c7aabd526371ec63ab2b99522fb
deadPanda11 <92713095+deadPanda11@users.noreply.github.com>
2022-03-01 19:43:39 +0600

    lab 10 added

---
+++ Lab Task 10_200042141/Employees/Employees.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees
+{
+    internal class Employees
+    {
+        public string id;
+        public string firstName;
+        public string lastName;
+        public string email;
+        public string phoneNumber;
+        public string hireDate;
+        public string jobId;
+        public string salary;
+        public string commission;
+        public string managerId;
+        public string departmentId;
+
+        public string getInfo()
+        {
+            return (id + "\t" + firstName);
+        }
+    }
+}
---
+++ Lab Task 10_200042141/Employees/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Employees
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EmployeesList = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.IdTextbox = new System.Windows.Forms.TextBox();
+            this.Salarylabel = new System.Windows.Forms.Label();
+            this.PhoneNoLabel = new System.Windows.Forms.Label();
+            this.IdLabel = new System.Windows.Forms.Label();
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // EmployeesList
+            // 
+            this.EmployeesList.FormattingEnabled = true;
+            this.EmployeesList.ItemHeight = 20;
+            this.EmployeesList.Location = new System.Drawing.Point(29, 40);
+            this.EmployeesList.Name = "EmployeesList";
+            this.EmployeesList.Size = new System.Drawing.Size(379, 484);
+            this.EmployeesList.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(497, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(497, 238);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(497, 292);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "ID:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(497, 347);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Phone Number:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(497, 402);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Salary:";
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Location = new System.Drawing.Point(591, 159);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(94, 29);
+            this.SearchButton.TabIndex = 6;
+            this.SearchButton.Text = "Search";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // IdTextbox
+            // 
+            this.IdTextbox.Location = new System.Drawing.Point(530, 98);
+            this.IdTextbox.Name = "IdTextbox";
+            this.IdTextbox.Size = new System.Drawing.Size(245, 27);
+            this.IdTextbox.TabIndex = 7;
+            // 
+            // Salarylabel
+            // 
+            this.Salarylabel.AutoSize = true;
+            this.Salarylabel.Location = new System.Drawing.Point(614, 402);
+            this.Salarylabel.Name = "Salarylabel";
+            this.Salarylabel.Size = new System.Drawing.Size(133, 20);
+            this.Salarylabel.TabIndex = 11;
+            this.Salarylabel.Text = "                               ";
+            // 
+            // PhoneNoLabel
+            // 
+            this.PhoneNoLabel.AutoSize = true;
+            this.PhoneNoLabel.Location = new System.Drawing.Point(614, 347);
+            this.PhoneNoLabel.Name = "PhoneNoLabel";
+            this.PhoneNoLabel.Size = new System.Drawing.Size(141, 20);
+            this.PhoneNoLabel.TabIndex = 10;
+            this.PhoneNoLabel.Text = "                                 ";
+            // 
+            // IdLabel
+            // 
+            this.IdLabel.AutoSize = true;
+            this.IdLabel.Location = new System.Drawing.Point(614, 292);
+            this.IdLabel.Name = "IdLabel";
+            this.IdLabel.Size = new System.Drawing.Size(141, 20);
+            this.IdLabel.TabIndex = 9;
+            this.IdLabel.Text = "                                 ";
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Location = new System.Drawing.Point(614, 238);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(105, 20);
+            this.NameLabel.TabIndex = 8;
+            this.NameLabel.Text = "                        ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(822, 548);
+            this.Controls.Add(this.Salarylabel);
+            this.Controls.Add(this.PhoneNoLabel);
+            this.Controls.Add(this.IdLabel);
+            this.Controls.Add(this.NameLabel);
+            this.Controls.Add(this.IdTextbox);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.EmployeesList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ListBox EmployeesList;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Button SearchButton;
+        private TextBox IdTextbox;
+        private Label Salarylabel;
+        private Label PhoneNoLabel;
+        private Label IdLabel;
+        private Label NameLabel;
+    }
+}
