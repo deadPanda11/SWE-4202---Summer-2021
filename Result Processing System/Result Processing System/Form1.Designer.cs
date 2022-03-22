69e20def1772bb0fe87fb95f2f033ab5774b4e6a
deadPanda11 <92713095+deadPanda11@users.noreply.github.com>
2022-03-22 19:46:43 +0600

    lab final

---
+++ Result Processing System/Result Processing System/Form1.Designer.cs
@@ -0,0 +1,424 @@
+﻿namespace Result_Processing_System
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InfoListbox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.quizTotalLabel = new System.Windows.Forms.Label();
+            this.quiz4Label = new System.Windows.Forms.Label();
+            this.quiz3Label = new System.Windows.Forms.Label();
+            this.quiz2Label = new System.Windows.Forms.Label();
+            this.quiz1Label = new System.Windows.Forms.Label();
+            this.attendanceLabel = new System.Windows.Forms.Label();
+            this.gradeLabel = new System.Windows.Forms.Label();
+            this.percentageLabel = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.vivaLabel = new System.Windows.Forms.Label();
+            this.finalLabel = new System.Windows.Forms.Label();
+            this.midLabel = new System.Windows.Forms.Label();
+            this.label25 = new System.Windows.Forms.Label();
+            this.label26 = new System.Windows.Forms.Label();
+            this.label28 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.idTextbox = new System.Windows.Forms.TextBox();
+            this.SearchByIdButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // InfoListbox
+            // 
+            this.InfoListbox.FormattingEnabled = true;
+            this.InfoListbox.ItemHeight = 20;
+            this.InfoListbox.Location = new System.Drawing.Point(26, 178);
+            this.InfoListbox.Name = "InfoListbox";
+            this.InfoListbox.Size = new System.Drawing.Size(439, 564);
+            this.InfoListbox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(599, 451);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Mid:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(599, 485);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Final:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(599, 525);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Viva:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(599, 631);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Grade:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(599, 591);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Percentage:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(599, 557);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(133, 20);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Total ( Out of 300):";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(599, 412);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(133, 20);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Quiz Total (Best 3):";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(599, 372);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(54, 20);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Quiz 4:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(599, 338);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(54, 20);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "Quiz 3:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(599, 306);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(54, 20);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Quiz 2:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(599, 266);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(54, 20);
+            this.label11.TabIndex = 8;
+            this.label11.Text = "Quiz 1:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(599, 232);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(88, 20);
+            this.label12.TabIndex = 7;
+            this.label12.Text = "Attendance:";
+            // 
+            // quizTotalLabel
+            // 
+            this.quizTotalLabel.AutoSize = true;
+            this.quizTotalLabel.Location = new System.Drawing.Point(738, 412);
+            this.quizTotalLabel.Name = "quizTotalLabel";
+            this.quizTotalLabel.Size = new System.Drawing.Size(153, 20);
+            this.quizTotalLabel.TabIndex = 24;
+            this.quizTotalLabel.Text = "                                    ";
+            // 
+            // quiz4Label
+            // 
+            this.quiz4Label.AutoSize = true;
+            this.quiz4Label.Location = new System.Drawing.Point(659, 372);
+            this.quiz4Label.Name = "quiz4Label";
+            this.quiz4Label.Size = new System.Drawing.Size(153, 20);
+            this.quiz4Label.TabIndex = 23;
+            this.quiz4Label.Text = "                                    ";
+            // 
+            // quiz3Label
+            // 
+            this.quiz3Label.AutoSize = true;
+            this.quiz3Label.Location = new System.Drawing.Point(659, 338);
+            this.quiz3Label.Name = "quiz3Label";
+            this.quiz3Label.Size = new System.Drawing.Size(153, 20);
+            this.quiz3Label.TabIndex = 22;
+            this.quiz3Label.Text = "                                    ";
+            // 
+            // quiz2Label
+            // 
+            this.quiz2Label.AutoSize = true;
+            this.quiz2Label.Location = new System.Drawing.Point(659, 306);
+            this.quiz2Label.Name = "quiz2Label";
+            this.quiz2Label.Size = new System.Drawing.Size(153, 20);
+            this.quiz2Label.TabIndex = 21;
+            this.quiz2Label.Text = "                                    ";
+            // 
+            // quiz1Label
+            // 
+            this.quiz1Label.AutoSize = true;
+            this.quiz1Label.Location = new System.Drawing.Point(659, 266);
+            this.quiz1Label.Name = "quiz1Label";
+            this.quiz1Label.Size = new System.Drawing.Size(153, 20);
+            this.quiz1Label.TabIndex = 20;
+            this.quiz1Label.Text = "                                    ";
+            // 
+            // attendanceLabel
+            // 
+            this.attendanceLabel.AutoSize = true;
+            this.attendanceLabel.Location = new System.Drawing.Point(693, 232);
+            this.attendanceLabel.Name = "attendanceLabel";
+            this.attendanceLabel.Size = new System.Drawing.Size(153, 20);
+            this.attendanceLabel.TabIndex = 19;
+            this.attendanceLabel.Text = "                                    ";
+            // 
+            // gradeLabel
+            // 
+            this.gradeLabel.AutoSize = true;
+            this.gradeLabel.Location = new System.Drawing.Point(657, 631);
+            this.gradeLabel.Name = "gradeLabel";
+            this.gradeLabel.Size = new System.Drawing.Size(153, 20);
+            this.gradeLabel.TabIndex = 18;
+            this.gradeLabel.Text = "                                    ";
+            // 
+            // percentageLabel
+            // 
+            this.percentageLabel.AutoSize = true;
+            this.percentageLabel.Location = new System.Drawing.Point(690, 591);
+            this.percentageLabel.Name = "percentageLabel";
+            this.percentageLabel.Size = new System.Drawing.Size(153, 20);
+            this.percentageLabel.TabIndex = 17;
+            this.percentageLabel.Text = "                                    ";
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.Location = new System.Drawing.Point(738, 557);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(153, 20);
+            this.totalLabel.TabIndex = 16;
+            this.totalLabel.Text = "                                    ";
+            // 
+            // vivaLabel
+            // 
+            this.vivaLabel.AutoSize = true;
+            this.vivaLabel.Location = new System.Drawing.Point(645, 525);
+            this.vivaLabel.Name = "vivaLabel";
+            this.vivaLabel.Size = new System.Drawing.Size(153, 20);
+            this.vivaLabel.TabIndex = 15;
+            this.vivaLabel.Text = "                                    ";
+            // 
+            // finalLabel
+            // 
+            this.finalLabel.AutoSize = true;
+            this.finalLabel.Location = new System.Drawing.Point(648, 485);
+            this.finalLabel.Name = "finalLabel";
+            this.finalLabel.Size = new System.Drawing.Size(153, 20);
+            this.finalLabel.TabIndex = 14;
+            this.finalLabel.Text = "                                    ";
+            // 
+            // midLabel
+            // 
+            this.midLabel.AutoSize = true;
+            this.midLabel.Location = new System.Drawing.Point(643, 451);
+            this.midLabel.Name = "midLabel";
+            this.midLabel.Size = new System.Drawing.Size(153, 20);
+            this.midLabel.TabIndex = 13;
+            this.midLabel.Text = "                                    ";
+            // 
+            // label25
+            // 
+            this.label25.AutoSize = true;
+            this.label25.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label25.Location = new System.Drawing.Point(770, 715);
+            this.label25.Name = "label25";
+            this.label25.Size = new System.Drawing.Size(120, 28);
+            this.label25.TabIndex = 26;
+            this.label25.Text = "200042141";
+            // 
+            // label26
+            // 
+            this.label26.AutoSize = true;
+            this.label26.Location = new System.Drawing.Point(659, 722);
+            this.label26.Name = "label26";
+            this.label26.Size = new System.Drawing.Size(105, 20);
+            this.label26.TabIndex = 25;
+            this.label26.Text = "Developed By:";
+            // 
+            // label28
+            // 
+            this.label28.AutoSize = true;
+            this.label28.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label28.Location = new System.Drawing.Point(67, 123);
+            this.label28.Name = "label28";
+            this.label28.Size = new System.Drawing.Size(343, 23);
+            this.label28.TabIndex = 27;
+            this.label28.Text = "Course Name: Object Oriented Concepts I";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Cambria Math", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.linkLabel1.LinkColor = System.Drawing.Color.Black;
+            this.linkLabel1.Location = new System.Drawing.Point(301, 5);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(329, 111);
+            this.linkLabel1.TabIndex = 28;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "IUT Result Processing System";
+            // 
+            // idTextbox
+            // 
+            this.idTextbox.Location = new System.Drawing.Point(599, 119);
+            this.idTextbox.Name = "idTextbox";
+            this.idTextbox.Size = new System.Drawing.Size(307, 27);
+            this.idTextbox.TabIndex = 29;
+            // 
+            // SearchByIdButton
+            // 
+            this.SearchByIdButton.Location = new System.Drawing.Point(743, 166);
+            this.SearchByIdButton.Name = "SearchByIdButton";
+            this.SearchByIdButton.Size = new System.Drawing.Size(163, 29);
+            this.SearchByIdButton.TabIndex = 30;
+            this.SearchByIdButton.Text = "Search By ID";
+            this.SearchByIdButton.UseVisualStyleBackColor = true;
+            this.SearchByIdButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(945, 795);
+            this.Controls.Add(this.SearchByIdButton);
+            this.Controls.Add(this.idTextbox);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label28);
+            this.Controls.Add(this.label25);
+            this.Controls.Add(this.label26);
+            this.Controls.Add(this.quizTotalLabel);
+            this.Controls.Add(this.quiz4Label);
+            this.Controls.Add(this.quiz3Label);
+            this.Controls.Add(this.quiz2Label);
+            this.Controls.Add(this.quiz1Label);
+            this.Controls.Add(this.attendanceLabel);
+            this.Controls.Add(this.gradeLabel);
+            this.Controls.Add(this.percentageLabel);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.vivaLabel);
+            this.Controls.Add(this.finalLabel);
+            this.Controls.Add(this.midLabel);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.InfoListbox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ListBox InfoListbox;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private Label quizTotalLabel;
+        private Label quiz4Label;
+        private Label quiz3Label;
+        private Label quiz2Label;
+        private Label quiz1Label;
+        private Label attendanceLabel;
+        private Label gradeLabel;
+        private Label percentageLabel;
+        private Label totalLabel;
+        private Label vivaLabel;
+        private Label finalLabel;
+        private Label midLabel;
+        private Label label25;
+        private Label label26;
+        private Label label28;
+        private LinkLabel linkLabel1;
+        private TextBox idTextbox;
+        private Button SearchByIdButton;
+    }
+}
